72389c7a68bb052d493b046bd1a37f29bc3b8b8c
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2023-04-19 01:31:12 -0300

    tentativa justinha
    try2

--- TryApp-0/src/app/app.component.ts
+++ TryApp-0/src/app/app.component.ts
@@ -11,13 +11,16 @@ import { IonicModule } from '@ionic/angular';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Pagina Inicial', url: '/folder/page', icon: 'mail' },
+      
+
+
+      ///aqui se guarda os objetos do menu
+
+
+
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ['Family', 'Friends'];
+  /// aqui as labels. ainda não sei como associar uma coisa a elas
   constructor() {}
 }
